ce82e7b20025d329d50a6a1f34ae80bc5e4e7032
Heather Park <hthr.prk@gmail.com>
2018-05-31 15:16:32 -0700

    Write render test for DrinkCustomizer

--- src/containers/DrinkCustomizer/DrinkCustomizer.test.js
+++ src/containers/DrinkCustomizer/DrinkCustomizer.test.js
@@ -7,21 +7,81 @@ import DrinkCustomizer from './DrinkCustomizer';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<DrinkCustomizer />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<DrinkCustomizer />);
+  });
+
   describe('rendering', () => {
-    it('should render base options as unordered list', () => {
-    
-    });
-  
-    it('should render topping options as unordered list', () => {
+    it('should render all options as unordered lists', () => {
+      const drinkOptions= {
+        bases: {
+          milkTea: [
+            {
+              flavor: 'green',
+              price: 2
+            },
+            {
+              flavor: 'black',
+              price: 2
+            },
+            {
+              flavor: 'oolong',
+              price: 2
+            }
+          ],
+          slush: [  
+            {
+              flavor: 'passionfruit',
+              price: 2
+            },
+            {
+              flavor: 'green apple',
+              price: 2
+            }
+          ]
+        },
+        toppings: [
+          {
+            name: 'boba',
+            price: 0.5
+          },
+          {
+            name: 'egg pudding',
+            price: 0.75
+          },
+          {
+            name: 'grass jelly',
+            price: 0.8
+          },
+        ],
+        ice: [0, 0.25, .5, .75, 1],
+        sugar: [0, 0.25, .5, .75, 1]
+      };
+      const countBases = (baseTypes) => {
+        let total = 0;
+
+        for (let type in baseTypes) {
+          total += baseTypes[type].length;
+        }
+        return total;
+      };
       
-    });
+      const baseTypeCount = Object.keys(drinkOptions.bases).length;
+      const baseCount = countBases(drinkOptions.bases);
+      const toppings = drinkOptions.toppings;
+      const iceLevels = drinkOptions.ice;
+      const sugarLevels = drinkOptions.sugar;
+
+      const listLength = wrapper.find('li').length;
+      const expectedListLength = baseTypeCount
+        + baseCount
+        + Object.keys(toppings).length
+        + Object.keys(iceLevels).length
+        + Object.keys(sugarLevels).length;
   
-    it('should render ice amounts as dropdown', () => {
-      
-    });
-  
-    it('should render sugar quantities as dropdown', () => {
-      
+      expect(listLength).toEqual(expectedListLength);
     });
   });
 
