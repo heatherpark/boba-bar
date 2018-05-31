d001c2f6171fa5cfa3e8b3b1a8d9eb454db6e9b0
Heather Park <hthr.prk@gmail.com>
2018-05-30 21:46:26 -0700

    Set up Drink component with prop types

---
+++ src/components/Drink/Drink.js
@@ -0,0 +1,15 @@
+import React from 'React';
+import PropTypes from 'prop-types';
+
+const Drink = props => {
+  return (
+    <div>
+    </div>
+  );  
+};
+
+Drink.propTypes = {
+  drinkorder: PropTypes.object
+};
+
+export default Drink;
