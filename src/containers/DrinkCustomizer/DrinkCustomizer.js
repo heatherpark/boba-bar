1e9a06d7e630479b1bb4cd641f8e009825d813e7
Heather Park <hthr.prk@gmail.com>
2018-05-31 15:16:46 -0700

    Write DrinkCustomizer code to make render test pass

--- src/containers/DrinkCustomizer/DrinkCustomizer.js
+++ src/containers/DrinkCustomizer/DrinkCustomizer.js
@@ -1,6 +1,51 @@
 import React, { Component } from 'react';
 
-class DrinkCustomizer extends Component() {
+const drinkOptions= {
+  bases: {
+    milkTea: [
+      {
+        flavor: 'green',
+        price: 2
+      },
+      {
+        flavor: 'black',
+        price: 2
+      },
+      {
+        flavor: 'oolong',
+        price: 2
+      }
+    ],
+    slush: [  
+      {
+        flavor: 'passionfruit',
+        price: 2
+      },
+      {
+        flavor: 'green apple',
+        price: 2
+      }
+    ]
+  },
+  toppings: [
+    {
+      name: 'boba',
+      price: 0.5
+    },
+    {
+      name: 'egg pudding',
+      price: 0.75
+    },
+    {
+      name: 'grass jelly',
+      price: 0.8
+    },
+  ],
+  ice: [0, 0.25, .5, .75, 1],
+  sugar: [0, 0.25, .5, .75, 1]
+};
+
+class DrinkCustomizer extends Component {
   state = {
     drinkOrder: {
       base: '',
@@ -13,12 +58,56 @@ class DrinkCustomizer extends Component() {
       sugar: 0
     }
   };
+
+  renderBases(bases) {
+    const baseTypes = []; 
+    
+    for (let type in bases) {
+      baseTypes.push(<li data-type="type">
+        {type} <ul>{renderBaseFlavors(bases[type])}</ul>
+      </li>);
+    }
+
+    return <ul>{baseTypes}</ul>;
+
+    function renderBaseFlavors(flavors) {
+      const baseFlavors = flavors.map(flavor => <li>{flavor.flavor}</li>);
+      return <ul>{baseFlavors}</ul>;
+    }
+  }
+  
+  renderToppings(toppings) {
+    const toppingElements = toppings.map(topping => (
+      <li>{topping.name} {topping.price}</li>
+    ));
+
+    return <ul>{toppingElements}</ul>;
+  }
   
   render() {
     return (
-      <div></div>
+      <div>
+        <div className="bases">
+          <p>bases:</p>
+          {this.renderBases(drinkOptions.bases)}
+        </div>
+        <div>
+          <p>toppings:</p>
+          {this.renderToppings(drinkOptions.toppings)}
+        </div>
+        <div>
+          <p>ice:</p>
+          {drinkOptions.ice.map(level => <li>{level}</li>)}
+        </div>
+        <div>
+          <p>sugar:</p>
+          {drinkOptions.sugar.map(level => <li>{level}</li>)}
+        </div>
+      </div>
     );  
   }
 }
+
+
 
 export default DrinkCustomizer;
