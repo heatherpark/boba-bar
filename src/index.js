2c1be473660291dde28191795039b43114f3be95
Heather Park <hthr.prk@gmail.com>
2018-06-01 12:41:24 -0700

    Wrap app in Redux provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import drinkCustomizerReducer from './store/reducers/drinkCustomizer/drinkCustomizer.reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  drinkCustomizerReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
