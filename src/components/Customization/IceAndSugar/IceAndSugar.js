abad706c2fa2ebc17d7b4caa59f5742cb151b4d4
Heather Park <hthr.prk@gmail.com>
2018-06-03 14:31:54 -0700

    Clean up IceAndSugar code

--- src/components/Customization/IceAndSugar/IceAndSugar.js
+++ src/components/Customization/IceAndSugar/IceAndSugar.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const IceAndSugar = props => {
   return (
-    <span onClick={() => props.chooseIceOrSugarLevel(props.item, props.level)}>
+    <span 
+      onClick={() => props.chooseIceOrSugarLevel(
+        props.item, 
+        props.level
+      )}>
       {props.level}
     </span>
   );
