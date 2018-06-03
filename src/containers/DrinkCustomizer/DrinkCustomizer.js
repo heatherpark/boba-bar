0727d6d963463807e1dfc2256deb5f7b2a22d7de
Heather Park <hthr.prk@gmail.com>
2018-06-03 10:18:22 -0700

    Extract CustomizerOptions

--- src/containers/DrinkCustomizer/DrinkCustomizer.js
+++ src/containers/DrinkCustomizer/DrinkCustomizer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Drink from '../../components/Drink/Drink';
+import CustomizerOptions from '../../components/CustomizerOptions/CustomizerOptions';
 import * as actions from '../../store/actions';
 
 export class DrinkCustomizer extends Component {
@@ -27,91 +28,22 @@ export class DrinkCustomizer extends Component {
     this.props.chooseIceOrSugarLevel(item, level);
   };
 
-  renderBases = bases => {
-    const baseTypes = [];
-
-    for (let type in bases) {
-      baseTypes.push(
-        <li key={type}>
-          {type} <ul>{this.renderBaseFlavors(bases[type])}</ul>
-        </li>
-      );
-    }
-
-    return <ul>{baseTypes}</ul>;
-  }
-
-  renderBaseFlavors = flavors => {
-    const baseFlavors = flavors.map(flavor => (
-      <li
-        key={flavor.flavor}
-        className="base-option"
-        onClick={() => {this.handleBaseClick(flavor.flavor, flavor.price)}}>
-        {flavor.flavor}
-      </li>
-    )
-    );
-
-    return <ul>{baseFlavors}</ul>;
-  }
-
-  renderIceAndSugarLevels = (item, levels) => {
-    const levelElements = levels.map(level =>
-      <li
-        className={`${item}-option`}
-        onClick={() => this.handleIceAndSugarLevelClick(item, level)}
-        key={level}>{level}</li>
-    );
-
-    return <ul>{levelElements}</ul>;
-  };
-
-  renderToppings(toppings) {
-    const toppingElements = toppings.map((topping, index) => (
-      <li key={topping.displayName}>
-        {topping.displayName} {topping.price}
-        <span
-          onClick={() => this.handleAddTopping(topping.value, topping.price)}
-          className="add">+</span>
-        <span
-          onClick={() => this.handleRemoveTopping(topping.value, topping.price)}
-          className="remove">-</span>
-      </li>
-    ));
-
-    return <ul>{toppingElements}</ul>;
-  }
-
-  renderDrinkOptions(drinkOptions) {
-    if (!drinkOptions) return 'Loading...';
-
-    return (
-      <div>
-        <div>
-          <p>bases:</p>
-          {this.renderBases(this.props.drinkOptions.bases)}
-        </div>
-        <div>
-          <p>toppings:</p>
-          {this.renderToppings(drinkOptions.toppings)}
-        </div>
-        <div>
-          <p>ice:</p>
-          {this.renderIceAndSugarLevels('ice', drinkOptions.ice)}
-        </div>
-        <div>
-          <p>sugar:</p>
-          {this.renderIceAndSugarLevels('sugar', drinkOptions.sugar)}
-        </div>
-      </div>
-    );
-  }
-
   render() {
     return (
       <div>
-        <Drink />
-        {this.renderDrinkOptions(this.props.drinkOptions)}
+        {this.props.drinkOptions
+          ? (<React.Fragment>
+                <Drink
+                  drinkOrder={this.props.drinkOrder}
+                  price={this.props.price} />
+                <CustomizerOptions
+                  chooseBase={this.props.chooseBase}
+                  addTopping={this.props.addTopping}
+                  removeTopping={this.props.removeTopping}
+                  chooseIceOrSugarLevel={this.props.chooseIceOrSugarLevel}
+                  drinkOptions={this.props.drinkOptions} />
+            </React.Fragment>)
+          : 'Loading options'}
       </div>
     );
   }
