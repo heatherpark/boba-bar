d2dc4b76b357e075b8570baa264e34d6e8cc4701
Heather Park <hthr.prk@gmail.com>
2018-06-03 12:53:51 -0700

    Remove unneeded import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import Drink from './components/Drink/Drink';
 import DrinkCustomizer from './containers/DrinkCustomizer/DrinkCustomizer';
 
 class App extends Component {
