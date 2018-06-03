6a8bc68aab7baa105e840cdca7d3b499d33fccbb
Heather Park <hthr.prk@gmail.com>
2018-06-03 14:30:15 -0700

    Clean up AddOn code

--- src/components/Customization/AddOn/AddOn.js
+++ src/components/Customization/AddOn/AddOn.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const AddOn = props => {
-  return (
-    <React.Fragment>
-      {props.displayName} {props.price}
-      <span
-        onClick={() => props.addAddOn(props.value, props.price)}
-        className="add">+</span>
-      <span
-        onClick={() => props.removeAddOn(props.value, props.price)}
-        className="remove">-</span>
-    </React.Fragment>
-  );
-};
+const AddOn = props => (
+  <React.Fragment>
+    {props.displayName} {props.price}
+    <span
+      onClick={() => props.addAddOn(props.value, props.price)}
+      className="add">+</span>
+    <span
+      onClick={() => props.removeAddOn(props.value, props.price)}
+      className="remove">-</span>
+  </React.Fragment>
+);
 
 export default AddOn;
