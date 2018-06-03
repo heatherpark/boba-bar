f7a88c05b85ba83e81c7799406d8623683a60336
Heather Park <hthr.prk@gmail.com>
2018-06-03 13:44:43 -0700

    Clean up CustomizerOptions code

--- src/components/CustomizerOptions/CustomizerOptions.js
+++ src/components/CustomizerOptions/CustomizerOptions.js
@@ -20,14 +20,15 @@ const CustomizerOptions = props => {
   }
 
   const renderBaseFlavors = flavors => {
-    const baseFlavors = flavors.map(flavor => (
-      <li key={flavor.flavor}>
-        <Base 
-          flavor={flavor.flavor}
-          price={flavor.price}
-          chooseBase={props.chooseBase} />
-      </li>
-    )
+    const baseFlavors = flavors.map(
+      flavor => (
+        <li key={flavor.flavor}>
+          <Base
+            flavor={flavor.flavor}
+            price={flavor.price}
+            chooseBase={props.chooseBase} />
+        </li>
+      )
     );
 
     return <ul>{baseFlavors}</ul>;
@@ -47,38 +48,32 @@ const CustomizerOptions = props => {
   };
 
   const renderAddOns = (addOns) => {
-    const elements = addOns.map((addOn, index) => (
-      <li key={addOn.displayName}>
-        <AddOn
-          addAddOn={props.addAddOn}
-          removeAddOn={props.removeAddOn}
-          displayName={addOn.displayName}
-          value={addOn.value}
-          price={addOn.price} />
-      </li>
-    ));
+    const elements = addOns.map(
+      (addOn, index) => (
+        <li key={addOn.displayName}>
+          <AddOn
+            addAddOn={props.addAddOn}
+            removeAddOn={props.removeAddOn}
+            displayName={addOn.displayName}
+            value={addOn.value}
+            price={addOn.price} />
+        </li>
+      )
+    );
 
     return <ul>{elements}</ul>;
   }
 
   return (
     <div>
-      <div>
-        <p>bases:</p>
-        {renderBases(props.drinkOptions.bases)}
-      </div>
-      <div>
-        <p>add-ons:</p>
-        {renderAddOns(props.drinkOptions.addOns)}
-      </div>
-      <div>
-        <p>ice:</p>
-        {renderIceAndSugarLevels('ice', props.drinkOptions.ice)}
-      </div>
-      <div>
-        <p>sugar:</p>
-        {renderIceAndSugarLevels('sugar', props.drinkOptions.sugar)}
-      </div>
+      <p>bases:</p>
+      {renderBases(props.drinkOptions.bases)}
+      <p>add-ons:</p>
+      {renderAddOns(props.drinkOptions.addOns)}
+      <p>ice:</p>
+      {renderIceAndSugarLevels('ice', props.drinkOptions.ice)}
+      <p>sugar:</p>
+      {renderIceAndSugarLevels('sugar', props.drinkOptions.sugar)}
     </div>
   );
 };
