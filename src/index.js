1d52430c33369c60f74de4a870154850e763f995
Heather Park <hthr.prk@gmail.com>
2018-06-05 14:55:09 -0700

    Wrap app in BrowserRouter hoc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
@@ -18,7 +19,9 @@ const store = createStore(
 
 const app = (
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
