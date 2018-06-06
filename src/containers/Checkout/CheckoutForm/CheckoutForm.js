e55b62ecf77c2c86e65148d0d7bd77b9e65034c5
Heather Park <hthr.prk@gmail.com>
2018-06-06 13:22:56 -0700

    Extract checkout form info to separate file

--- src/containers/Checkout/CheckoutForm/CheckoutForm.js
+++ src/containers/Checkout/CheckoutForm/CheckoutForm.js
@@ -2,94 +2,17 @@ import React, { Component } from 'react';
 
 import Input from '../../../components/UI/Input/Input';
 import { checkValidity } from '../../../shared/utility';
+import checkoutFormData from './checkout-form-data';
 
 class CheckoutForm extends Component {
   state = {
-    checkoutForm: {
-      name: {
-        elementType: 'input',
-        elementConfig: {
-          type: 'text',
-          placeholder: 'Your Name'
-        },
-        value: '',
-        validation: {
-          required: true
-        },
-        valid: false,
-        touched: false
-      },
-      street: {
-        elementType: 'input',
-        elementConfig: {
-          type: 'text',
-          placeholder: 'Street'
-        },
-        value: '',
-        validation: {
-          required: true
-        },
-        valid: false,
-        touched: false
-      },
-      zipCode: {
-        elementType: 'input',
-        elementConfig: {
-          type: 'text',
-          placeholder: 'ZIP Code'
-        },
-        value: '',
-        validation: {
-          required: true,
-          minLength: 5,
-          maxLength: 5,
-          isNumeric: true
-        },
-        valid: false,
-        touched: false
-      },
-      country: {
-        elementType: 'input',
-        elementConfig: {
-          type: 'text',
-          placeholder: 'Country'
-        },
-        value: '',
-        validation: {
-          required: true
-        },
-        valid: false,
-        touched: false
-      },
-      email: {
-        elementType: 'input',
-        elementConfig: {
-          type: 'email',
-          placeholder: 'Your E-Mail'
-        },
-        value: '',
-        validation: {
-          required: true,
-          isEmail: true
-        },
-        valid: false,
-        touched: false
-      },
-      deliveryMethod: {
-        elementType: 'select',
-        elementConfig: {
-          options: [
-            { value: 'fastest', displayValue: 'Fastest' },
-            { value: 'cheapest', displayValue: 'Cheapest' }
-          ]
-        },
-        value: 'fastest',
-        validation: {},
-        valid: true
-      }      
-    },
+    checkoutForm: {},
     formIsValid: false
   };
+
+  componentWillMount() {
+    this.setState({ checkoutForm: checkoutFormData });
+  }
 
   handleInputChange = (event, inputIdentifier) => {
     const updatedFormElement = {
