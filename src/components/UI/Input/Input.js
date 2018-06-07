dc43cc6e230cc549755b932a92e1ea0fd3284bb1
Heather Park <hthr.prk@gmail.com>
2018-06-07 14:38:49 -0700

    Add styling for Input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,26 +1,36 @@
 import React from 'react';
 
+import classes from './Input.css';
+
 const input = (props) => {
   let inputElement = null;
+  const inputClasses = [classes.InputElement];
+
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid);
+  }
+
   switch (props.elementType) {
     case ('input'):
       inputElement = <input
-        // className={inputClasses.join(' ')}
+      className={inputClasses.join(' ')}
         {...props.elementConfig}
         value={props.value}
         onChange={props.onChange} />;
       break;
+
     case ('textarea'):
       inputElement = <textarea
-        // className={inputClasses.join(' ')}
+        className={inputClasses.join(' ')}
         {...props.elementConfig}
         value={props.value}
         onChange={props.onChange} />;
       break;
+
     case ('select'):
       inputElement = (
         <select
-          // className={inputClasses.join(' ')}
+          className={inputClasses.join(' ')}
           value={props.value}
           onChange={props.onChange}>
           {props.elementConfig.options.map(option => (
@@ -31,9 +41,10 @@ const input = (props) => {
         </select>
       );
       break;
+      
     default:
       inputElement = <input
-        // className={inputClasses.join(' ')}
+        className={inputClasses.join(' ')}
         {...props.elementConfig}
         value={props.value}
         onChange={props.onChange} />;
