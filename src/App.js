d511454c48473164676c636b86a55ffa58ce8c1c
Heather Park <hthr.prk@gmail.com>
2018-06-07 14:54:18 -0700

    Update checkout route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import DrinkCustomizer from './containers/DrinkCustomizer/DrinkCustomizer';
-import CheckoutForm from './containers/Checkout/CheckoutForm/CheckoutForm';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route path="/checkout" exact component={CheckoutForm} />
+          <Route path="/checkout" exact component={Checkout} />
           <Route path="/" exact component={DrinkCustomizer} />
           <Redirect to="/" />
         </Switch>
