ea8df437992d94026468c3ee26b059115fac5c42
Heather Park <hthr.prk@gmail.com>
2018-06-07 15:01:49 -0700

    Handle canceling checkout

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import Modal from '../../components/UI/Modal/Modal';
 import CheckoutForm from './CheckoutForm/CheckoutForm';
 
 class Checkout extends Component {
   state = {
-    checkingOut: false
+    checkingOut: false,
+    checkoutCanceled: false
   };
 
   handleCheckoutContinued = () => {
     this.setState({ checkingOut: true });
   };
 
+  handleCheckoutCanceled = () => {
+    this.setState({ checkoutCanceled: true });
+  };
+
   render() {
     return (
       <div>
+        {this.state.checkoutCanceled ? <Redirect to="/" /> : null}
+
         <button onClick={this.handleCheckoutContinued}>check out</button>
-        <button>cancel</button>
+        <button onClick={this.handleCheckoutCanceled}>cancel</button>
+        
         <Modal showModal={this.state.checkingOut}>
           <CheckoutForm />
         </Modal>
