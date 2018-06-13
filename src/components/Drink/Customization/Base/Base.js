271ee5b8c426aec512b52f18c58acd2a5d6158e1
Heather Park <hthr.prk@gmail.com>
2018-06-12 21:24:16 -0700

    Clean up

--- src/components/Drink/Customization/Base/Base.js
+++ src/components/Drink/Customization/Base/Base.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Base = props => (
   <span
     className="base-option"
-    onClick={() => props.onChooseBase(props.type, props.children, props.price)}>
+    onClick={() => props.onChooseBase(
+      props.type, 
+      props.children, 
+      props.price)}>
     {props.children}
   </span>
 );
--- src/components/Drink/Customization/IceAndSugar/IceAndSugar.js
+++ src/components/Drink/Customization/IceAndSugar/IceAndSugar.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-const IceAndSugar = props => {
-  return (
-    <span 
-      onClick={() => props.onChooseIceOrSugarLevel(
-        props.item, 
-        props.level
-      )}>
-      {props.level}
-    </span>
-  );
-};
+const IceAndSugar = props => (
+  <span
+    onClick={() => props.onChooseIceOrSugarLevel(
+      props.item,
+      props.level)}>
+    {props.level}
+  </span>
+);
 
 export default IceAndSugar;
