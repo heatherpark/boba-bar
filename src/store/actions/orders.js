3b18e39c7da7558101431f775cc429f7e5c1e83b
Heather Park <hthr.prk@gmail.com>
2018-06-12 20:10:40 -0700

    Clean up orders action creators

--- src/store/actions/orders.js
+++ src/store/actions/orders.js
@@ -2,34 +2,25 @@ import * as actionTypes from './actionTypes';
 import axios from '../../axios/orders';
 
 export const checkOut = drinkOrder => {
-  return dispatch => {
-    dispatch(checkoutStart());
-
-    axios.post('/orders.json', drinkOrder)
-      .then(response => {
-        dispatch(checkoutSuccess());
-      })
-      .catch(error => {
-        console.error(error);
-        dispatch(checkoutFailed());
-      });
+  return async function (dispatch) {
+    try {
+      const response = await axios.post('/orders.json', drinkOrder);
+      dispatch(checkoutSuccess());
+    } catch (error) {
+      console.error(error);
+      dispatch(checkoutFailed());
+    }
   };
 };
 
-export const checkoutStart = () => {
-  return {
-    type: actionTypes.CHECKOUT_START
-  };
-};
+export const checkoutStart = () => ({
+  type: actionTypes.CHECKOUT_START
+});
 
-export const checkoutSuccess = () => {
-  return {
-    type: actionTypes.CHECKOUT_SUCCESS
-  };
-};
+export const checkoutSuccess = () => ({
+  type: actionTypes.CHECKOUT_SUCCESS
+});
 
-export const checkoutFailed = () => {
-  return {
-    type: actionTypes.CHECKOUT_FAILED
-  };
-};
+export const checkoutFailed = () => ({
+  type: actionTypes.CHECKOUT_FAILED
+});
