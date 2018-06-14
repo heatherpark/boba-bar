c544b5e5b9cd197e05988e3fc0ccff5a99675914
Heather Park <hthr.prk@gmail.com>
2018-06-14 08:29:48 -0700

    Add action for login expiration

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -4,7 +4,7 @@ import * as utils from './utilities';
 import * as actionTypes from './actionTypes';
 
 export const auth = (email, password, isSignup) => {
-  return async function(dispatch) {
+  return async function (dispatch) {
     dispatch(authStart());
 
     const authData = {
@@ -14,22 +14,28 @@ export const auth = (email, password, isSignup) => {
     };
 
     try {
-      const response = isSignup 
+      const response = isSignup
         ? await axios.post('/signupNewUser', authData)
         : await axios.post('/verifyPassword', authData);
-      const { expiresIn, idToken: token, localId: userId } = response.data;
+      const { 
+        expiresIn, 
+        idToken: token, 
+        localId: userId,
+      } = response.data;
 
       utils.setAuthData(token, userId, expiresIn);
 
       dispatch(authSuccess(token, userId));
+      dispatch(checkAuthTimeout(expiresIn));
     } catch (error) {
-      console.error(error);
+      console.error(error.response.data.error.message);
+      dispatch(authFailed(error.response.data.error.message));
     }
   }
 };
 
 export const authStart = () => ({
-  type: actionTypes.AUTH_START 
+  type: actionTypes.AUTH_START
 });
 
 export const authSuccess = (token, userId) => ({
@@ -37,6 +43,19 @@ export const authSuccess = (token, userId) => ({
   token,
   userId
 });
+
+export const authFailed = error => ({
+  type: actionTypes.AUTH_FAILED,
+  error
+});
+
+export const checkAuthTimeout = (expirationTime) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
 
 export const logout = () => ({
   type: actionTypes.AUTH_LOGOUT
