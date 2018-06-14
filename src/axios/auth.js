3f52b97e87d5c4909410500502cf30d4f88ffd22
Heather Park <hthr.prk@gmail.com>
2018-06-13 21:12:18 -0700

    Add axios auth instance

---
+++ src/axios/auth.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { FIREBASE_AUTH } from '../config/client';
+
+const { API_KEY } = FIREBASE_AUTH;
+
+const instance = axios.create({
+  baseURL: `https://www.googleapis.com/identitytoolkit/v3/relyingparty?key=${API_KEY}`
+});
+
+export default instance;
