e040ba9111bec7528416c11d559965103c20ee12
Heather Park <hthr.prk@gmail.com>
2018-06-13 19:31:50 -0700

    Write tests for orders reducer

--- src/store/reducers/orders/orders.test.js
+++ src/store/reducers/orders/orders.test.js
@@ -24,6 +24,7 @@ describe('orders reducer', () => {
     const nextState = reducer(initialState, action);
 
     const expectedState = {
+      ...initialState,
       checkedOut: true,
       checkingOut: false
     };
@@ -43,8 +44,54 @@ describe('orders reducer', () => {
     const nextState = reducer(initialState, action);
 
     const expectedState = {
+      ...initialState,
       checkedOut: false,
       checkingOut: false
+    };
+
+    expect(nextState).toEqual(expectedState);
+  });
+
+  it('should handle FETCH_ORDERS_START', () => {
+    const action = {
+      type: actionTypes.FETCH_ORDERS_START
+    };
+    const nextState = reducer(initialState, action);
+
+    const expectedState = {
+      ...initialState,
+      fetchingOrders: true
+    };
+
+    expect(nextState).toEqual(expectedState);
+  });
+
+  it('should handle FETCH_ORDERS_SUCCESS', () => {
+    const orders = [{}, {}];
+    const action = {
+      type: actionTypes.FETCH_ORDERS_SUCCESS,
+      orders
+    };
+    const nextState = reducer(initialState, action);
+
+    const expectedState = {
+      ...initialState,
+      fetchingOrders: false,
+      orders
+    };
+
+    expect(nextState).toEqual(expectedState);
+  });
+
+  it('should handle FETCH_ORDERS_FAILED', () => {
+    const action = {
+      type: actionTypes.FETCH_ORDERS_FAILED
+    };
+    const nextState = reducer(initialState, action);
+
+    const expectedState = {
+      ...initialState,
+      fetchingOrders: false,
     };
 
     expect(nextState).toEqual(expectedState);
