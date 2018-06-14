647981dca6a9774a49e058d3e4483249c5225f77
Heather Park <hthr.prk@gmail.com>
2018-06-13 19:45:34 -0700

    Set up Orders component

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../store/actions/index';
+
+export class Orders extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  orders: state.orders.orders
+});
+
+const mapDispatchToProps = dispatch => ({
+  onFetchOrders: () => dispatch(actions.fetchOrders)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);
