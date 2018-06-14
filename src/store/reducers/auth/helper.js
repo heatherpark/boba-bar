f727e23fed4772461fb45dba35335759758ada3a
Heather Park <hthr.prk@gmail.com>
2018-06-13 20:27:53 -0700

    Write auth reducer

--- src/store/reducers/auth/helper.js
+++ src/store/reducers/auth/helper.js
@@ -4,3 +4,29 @@ export const initialState = {
   loggingIn: false,
   error: null
 };
+
+export const authStart = (state, action) => ({
+  ...state,
+  error: null,
+  loggingIn: true
+});
+
+export const authSuccess = (state, action) => ({
+  ...state,
+  token: action.token,
+  userId: action.userId,
+  error: null,
+  loggingIn: false
+});
+
+export const authFailed = (state, action) => ({
+  ...state,
+  error: action.error,
+  loggingIn: false
+});
+
+export const authLogout = (state, action) => ({
+  ...state,
+  token: null,
+  userId: null
+});
