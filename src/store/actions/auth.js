77a51dff7d5cc85bee616d25ed99f4928a8cb508
Heather Park <hthr.prk@gmail.com>
2018-06-16 12:00:35 -0700

    Clean up code

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,6 +1,28 @@
 import axios from '../../axios/auth';
-import * as utils from './utilities';
 import * as actionTypes from './actionTypes';
+
+const setAuthData = (token, userId, expiresIn) => {
+  const expirationDate = new Date(
+    new Date().getTime() + expiresIn * 1000
+  );
+  
+  localStorage.setItem('token', token);
+  localStorage.setItem('userId', userId);
+  localStorage.setItem('expirationDate', expirationDate);
+};
+
+export const authStart = () => ({ type: actionTypes.AUTH_START });
+
+export const authSuccess = (token, userId) => ({
+  type: actionTypes.AUTH_SUCCESS,
+  token,
+  userId
+});
+
+export const authFail = error => ({
+  type: actionTypes.AUTH_FAIL,
+  error
+});
 
 export const auth = (email, password, isSignup) => {
   return async dispatch => {
@@ -18,7 +40,7 @@ export const auth = (email, password, isSignup) => {
         : await axios.post('/verifyPassword', authData);
       const { expiresIn, idToken: token, localId: userId } = response.data;
 
-      utils.setAuthData(token, userId, expiresIn);
+      setAuthData(token, userId, expiresIn);
 
       dispatch(authSuccess(token, userId));
     } catch (error) {
@@ -27,16 +49,3 @@ export const auth = (email, password, isSignup) => {
     }
   }
 };
-
-export const authStart = () => ({ type: actionTypes.AUTH_START });
-
-export const authSuccess = (token, userId) => ({
-  type: actionTypes.AUTH_SUCCESS,
-  token,
-  userId
-});
-
-export const authFail = error => ({
-  type: actionTypes.AUTH_FAIL,
-  error
-});
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -8,6 +8,7 @@ export {
 
 export { 
   checkOut,
-  fetchOrders } from './orders';
+  fetchOrders 
+} from './orders';
 
 export { auth } from './auth';
