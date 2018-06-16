3b937545a8e25b1a0b3b3076c45930c57b2689a0
Heather Park <hthr.prk@gmail.com>
2018-06-16 12:00:40 -0700

    Remove unnecessary file

--- src/store/actions/utilities.js
+++
@@ -1,7 +0,0 @@
-export const setAuthData = (token, userId, expiresIn) => {
-  const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
-  
-  localStorage.setItem('token', token);
-  localStorage.setItem('userId', userId);
-  localStorage.setItem('expirationDate', expirationDate);
-};
