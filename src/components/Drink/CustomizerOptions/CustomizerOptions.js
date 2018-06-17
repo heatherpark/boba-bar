4cefb046d8e788200819e02dff96dc6876dc297a
Heather Park <hthr.prk@gmail.com>
2018-06-17 16:27:26 -0700

    Update styling

--- src/components/Drink/CustomizerOptions/CustomizerOptions.js
+++ src/components/Drink/CustomizerOptions/CustomizerOptions.js
@@ -4,79 +4,89 @@ import Base from '../Customization/Base/Base';
 import AddOn from '../Customization/AddOn/AddOn';
 import IceAndSugar from '../Customization/IceAndSugar/IceAndSugar';
 
+import { Segment, List, Header } from 'semantic-ui-react';
+
 const CustomizerOptions = props => {
   const renderBases = bases => {
     const baseTypes = [];
 
     for (let type in bases) {
       baseTypes.push(
-        <li key={type}>
-          {type}: {bases[type].price} <ul>{renderBaseFlavors(type, bases[type])}</ul>
-        </li>
+        <List.Item key={type}>
+          {type} ${bases[type].price} {renderBaseFlavors(type, bases[type])}
+        </List.Item>
       );
     }
 
-    return <ul>{baseTypes}</ul>;
+    return <List>{baseTypes}</List>;
   }
 
   const renderBaseFlavors = (baseType, baseData) => {
     const elements = baseData.flavors.map(
       flavor => (
-        <li key={baseType + '-' + flavor}>
+        <List.Item key={baseType + '-' + flavor}>
           <Base
             type={baseType}
             price={baseData.price}
             onChooseBase={props.onChooseBase}>
             {flavor}
           </Base>
-        </li>
+        </List.Item>
       )
     );
 
-    return <ul>{elements}</ul>;
+    return <List horizontal>{elements}</List>;
   }
 
   const drinkOrderIsValid = drinkOrder => drinkOrder && drinkOrder.base.flavor;
 
   const renderIceAndSugarLevels = (item, levels) => {
     const levelElements = levels.map(level =>
-      <li key={`${item}-${level}`}>
+      <List.Item key={`${item}-${level}`}>
         <IceAndSugar
           onChooseIceOrSugarLevel={props.onChooseIceOrSugarLevel}
           item={item}
           level={level} />
-      </li>
+      </List.Item> 
     );
 
-    return <ul>{levelElements}</ul>;
+    return <List horizontal>{levelElements}</List>;
   };
 
   const renderAddOns = (addOns) => {
     const elements = addOns.map(addOn => (
-      <li key={addOn.displayName}>
+      <List.Item key={addOn.displayName}>
         <AddOn
           onAddAddOn={props.onAddAddOn}
           onRemoveAddOn={props.onRemoveAddOn}
           displayName={addOn.displayName}
           value={addOn.value}
           price={addOn.price} />
-      </li>
+      </List.Item>
     )
     );
 
-    return <ul>{elements}</ul>;
+    return <List horizontal>{elements}</List>;
   }
 
   return (
     <div>
-      <p>bases:</p>
-      {renderBases(props.drinkOptions.bases)}
-      <p>add-ons:</p>
-      {renderAddOns(props.drinkOptions.addOns)}
-      <p>ice:</p>
-      {renderIceAndSugarLevels('ice', props.drinkOptions.ice)}
-      <p>sugar:</p>
-      {renderIceAndSugarLevels('sugar', props.drinkOptions.sugar)}
+      <Segment>
+        <Header as="h3">choose your base:</Header>
+        {renderBases(props.drinkOptions.bases)}
+      </Segment>
+      <Segment>
+        <Header as="h3">add-ons:</Header>
+        {renderAddOns(props.drinkOptions.addOns)}
+      </Segment>
+      <Segment>
+        <Header as="h3">ice:</Header>
+        {renderIceAndSugarLevels('ice', props.drinkOptions.ice)}
+      </Segment>
+      <Segment>
+        <Header as="h3">sugar:</Header>
+        {renderIceAndSugarLevels('sugar', props.drinkOptions.sugar)}
+      </Segment>
       <button
         onClick={props.onPlaceOrder}
         disabled={!drinkOrderIsValid(props.drinkOrder)}>Buy Drink</button>
