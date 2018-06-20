f2e84496be6f35a8b86e3056d9a560f23771496b
Heather Park <hthr.prk@gmail.com>
2018-06-20 08:40:03 -0700

    Add authRedirectPath state prop

--- src/containers/Auth/Login/Login.js
+++ src/containers/Auth/Login/Login.js
@@ -112,7 +112,8 @@ class Login extends Component {
 
 const mapStateToProps = state => {
   return {
-    isAuthenticated: state.auth.token !== null
+    isAuthenticated: state.auth.token !== null,
+    authRedirectPath: state.auth.authRedirectPath
   };
 };
 
