752f7503fd8f2811b02877a12bbd0fc8b5ac486c
Heather Park <hthr.prk@gmail.com>
2018-06-21 11:32:52 -0700

    Add double binding

--- src/containers/Checkout/CheckoutForm/CheckoutForm.js
+++ src/containers/Checkout/CheckoutForm/CheckoutForm.js
@@ -23,7 +23,7 @@ class CheckoutForm extends Component {
 
   render() {
     const checkoutFormKey = 'checkoutForm';
-    const checkoutForm = this.state.checkoutForm;
+    const checkoutFormConfig = this.state.checkoutForm;
 
     return (
       <div style={{ padding: '4rem' }}>
@@ -36,51 +36,57 @@ class CheckoutForm extends Component {
               fluid
               label="First name"
               placeholder="First name"
+              value={checkoutFormConfig.firstName.value}
               onFocus={() => this.markInputAsTouched(checkoutFormKey, 'firstName')}
               onChange={e => this.handleInputChange(e.target.value, checkoutFormKey, 'firstName')}
-              error={fieldIsInvalid(checkoutForm, 'firstName')} />
+              error={fieldIsInvalid(checkoutFormConfig, 'firstName')} />
             <Form.Input
               fluid
               label="Last name"
               placeholder="Last name"
+              value={checkoutFormConfig.lastName.value}
               onFocus={() => this.markInputAsTouched(checkoutFormKey, 'lastName')}
               onChange={e => this.handleInputChange(e.target.value, checkoutFormKey, 'lastName')}
-              error={fieldIsInvalid(checkoutForm, 'lastName')} />
+              error={fieldIsInvalid(checkoutFormConfig, 'lastName')} />
           </Form.Group>
           <Form.Input
             fluid
             label="Address"
             placeholder="Address"
+            value={checkoutFormConfig.address.value}
             onFocus={() => this.markInputAsTouched(checkoutFormKey, 'address')}
             onChange={e => this.handleInputChange(e.target.value, checkoutFormKey, 'address')}
-            error={fieldIsInvalid(checkoutForm, 'address')} />
+            error={fieldIsInvalid(checkoutFormConfig, 'address')} />
           <Form.Group>
             <Form.Input
               width={8}
               label="City"
               placeholder="City"
+              value={checkoutFormConfig.city.value}
               onFocus={() => this.markInputAsTouched(checkoutFormKey, 'city')}
               onChange={e => this.handleInputChange(e.target.value, checkoutFormKey, 'city')}
-              error={fieldIsInvalid(checkoutForm, 'city')} />
+              error={fieldIsInvalid(checkoutFormConfig, 'city')} />
             <Form.Input
               width={3}
               label="State"
               placeholder="State"
+              value={checkoutFormConfig.state.value}
               onFocus={() => this.markInputAsTouched(checkoutFormKey, 'state')}
               onChange={e => this.handleInputChange(e.target.value, checkoutFormKey, 'state')}
-              error={fieldIsInvalid(checkoutForm, 'state')} />
+              error={fieldIsInvalid(checkoutFormConfig, 'state')} />
             <Form.Input
               wdith={5}
               label="Zip code"
               placeholder="Zip code"
+              value={checkoutFormConfig.zipCode.value}
               onFocus={() => this.markInputAsTouched(checkoutFormKey, 'zipCode')}
               onChange={e => this.handleInputChange(e.target.value, checkoutFormKey, 'zipCode')}
-              error={fieldIsInvalid(checkoutForm, 'zipCode')} />
+              error={fieldIsInvalid(checkoutFormConfig, 'zipCode')} />
           </Form.Group>
 
           <Form.Button
             disabled={!this.state.formIsValid}
-            onClick={event => this.props.onOrderSubmit(event, checkoutForm)}>
+            onClick={event => this.props.onOrderSubmit(event, checkoutFormConfig)}>
             Complete order
           </Form.Button>
         </Form>
