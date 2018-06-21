aa3f1c3d631f0dec387b6c6f510d83131613fa56
Heather Park <hthr.prk@gmail.com>
2018-06-21 16:03:57 -0700

    Remove test tile

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'
 
 import * as actions from '../../store/actions/index';
 import Order from '../../components/Order/Order';
